6b6a00cca06e21f5cdaae716a994d49e4ed1c915
Muhammad Robitulloh <67168926+muhammad-robitulloh@users.noreply.github.com>
2025-06-26 18:08:15 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cognitiveshell',
+    name='neuronet-ai-cognitiveshell',
     version='0.2.1',
     packages=find_packages(),
     install_requires=[
